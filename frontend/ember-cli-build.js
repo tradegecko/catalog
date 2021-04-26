6ab9bb24a4d2d5a53d5c1e4bd739f5c72c87c819
Sebastien Stettler <sebastien_stettler@intuit.com>
2021-04-26 15:43:02 +0800

    some ui

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -6,6 +6,11 @@ module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     'ember-math-helpers': {
       only: ['add', 'sub'],
+    },
+
+    'ember-bootstrap': {
+      bootstrapVersion: 4,
+      importBootstrapCSS: false
     }
   });
 
