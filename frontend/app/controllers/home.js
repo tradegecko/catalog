4ac2adcfae4c16ca825a9a542bc12e890a230929
Sebastien Stettler <sebastien_stettler@intuit.com>
2021-04-28 20:54:14 +0800

    add filters to page

--- frontend/app/controllers/home.js
+++ frontend/app/controllers/home.js
@@ -1,16 +1,26 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class HomeController extends Controller {
+  @tracked channels;
+  @tracked filters;
+
   @service store;
-  queryParams = ['channel_id']
+
+  queryParams = ['channel_id', 'filter_id']
+
+  constructor(){
+    super(...arguments);
+    this.channels =this.store.findAll('channel');
+    this.filters =this.store.query('filterTab', {page: 'variants'});
+  }
 
   async fetchChannel() {
     if (this.channel_id) {
       let channels = await this.store.query('channel', {
         ids: [this.channel_id],
       });
-      this.set('channel', channels.firstObject);
     }
   }
 }
